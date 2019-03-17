45c5da68180b471ed70f1a4104a9dcc99a63fd09
phucngovan <phucdepzai0712@gmail.com>
2019-03-17 20:23:25 +0700

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddProductsComponent } from './add-products/add-products.component';
+import { ListComponent } from './list/list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddProductsComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
