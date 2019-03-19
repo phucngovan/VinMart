034061b9f5b8ca7df5366ffe5d75b9b574a8de68
phucngovan <phucdepzai0712@gmail.com>
2019-03-19 10:33:05 +0700

    test validate name

--- src/app/add-products/add-products.component.ts
+++ src/app/add-products/add-products.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup} from '@angular/forms';
+import {FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
 import {Router} from '@angular/router';
 import {ProductsService} from '../products.service';
 
@@ -11,16 +11,27 @@ import {ProductsService} from '../products.service';
 export class AddProductsComponent implements OnInit {
 
   constructor(private formBuilder: FormBuilder, private router: Router, private productsService: ProductsService) { }
-  addForm: FormGroup;
+
+  // addForm: FormGroup;
+  addForm = new FormGroup({
+    id: new FormControl(''),
+    name: new FormControl('',[
+      Validators.required,
+      Validators.minLength(6)
+    ]),
+    price: new FormControl(''),
+    address: new FormControl(''),
+    description: new FormControl('')
+  });
 
   ngOnInit() {
-    this.addForm = this.formBuilder.group({
-      id: [],
-      name: [],
-      price: [],
-      address: [],
-      description: []
-    });
+    // this.addForm = this.formBuilder.group({
+    //   id: [],
+    //   name: [Validators.required,Validators.minLength(6)],
+    //   price: [],
+    //   address: [],
+    //   description: []
+    // });
   }
   onSubmit() {
     this.productsService.addProduct(this.addForm.value)
